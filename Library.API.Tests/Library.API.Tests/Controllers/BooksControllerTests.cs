376b5728d071104292680ad1130705bd87b95744
Leandro de Araújo Souza <leandro.souza@smait.com.br>
2020-08-26 23:23:36 -0300

    Create BooksControllerTests

---
+++ Library.API.Tests/Library.API.Tests/Controllers/BooksControllerTests.cs
@@ -0,0 +1,33 @@
+﻿using FluentAssertions;
+using Library.API.Controllers;
+using Library.API.Models;
+using Library.API.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace Library.API.Tests.Controllers
+{
+    [TestClass]
+    public class BooksControllerTests
+    {
+        private readonly Mock<IBooksService> _mockBookService;
+        private readonly BooksController _controller;
+
+        public BooksControllerTests()
+        {
+            _mockBookService = new Mock<IBooksService>();
+            _controller = new BooksController(_mockBookService.Object);
+        }
+
+        [TestMethod]
+        public void Create_NewBook_ShouldReturnOkObjectResult()
+        {
+            var book = new Mock<BookToCreate>();
+
+            var result = _controller.Add(book.Object);
+
+            result.Should().BeOfType<OkObjectResult>();
+        }
+    }
+}
